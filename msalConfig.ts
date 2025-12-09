a4687461215896d87d8f90c8f96f62546efe8197
pushpaanand <productananlyst.pushpa@kauveryhospital.com>
2025-12-09 15:22:55 +0530

    client id error fixed

--- msalConfig.ts
+++ msalConfig.ts
@@ -1,9 +1,10 @@
 import { Configuration, RedirectRequest } from '@azure/msal-browser';
 
 // const env = (import.meta as any)?.env || {};
-const tenantId = process.env.VITE_AAD_TENANT_ID || '';
-const clientId = process.env.VITE_AAD_CLIENT_ID || '';
-const redirectUri = process.env.VITE_AAD_REDIRECT_URI || (typeof window !== 'undefined' ? window.location.origin : '');
+const env       = (import.meta as unknown as { env: Record<string, string> })?.env || {};
+const tenantId  = env.VITE_AAD_TENANT_ID  || '767a4f7b-5957-4143-8bd4-b152154fe7f6';
+const clientId  = env.VITE_AAD_CLIENT_ID  || 'bb140565-7dd0-4f93-8336-b2b4bb843580';
+const redirectUri = env.VITE_AAD_REDIRECT_URI || window.location.origin || 'https://riskregister.kauverykonnect.com';
 
 export const msalConfig: Configuration = {
   auth: {
